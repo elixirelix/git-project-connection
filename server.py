07f3310673ddc83fb10f1f15a5cf3e69317f73fc
elixirelix <nathanael.martin210918@gmail.com>
2026-08-15 20:49:27 +0200

    fix: fixing start all services

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from uvicorn import Config, Server
 app = FastAPI()
 
 @app.post("/github/push")
@@ -14,4 +15,12 @@ async def root(req: dict):
     print(req)
     return {"status": "ok"}
 
+async def start_server():
+    config = Config(
+        app,
+        host="0.0.0.0",
+        port=8000
+    )
+    server = Server(config)
+    await server.serve()
 
