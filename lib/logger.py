c666106cd950593f3113636df498604989ca8012
elixirelix <nathanael.martin210918@gmail.com>
2026-08-15 20:36:03 +0200

    fix: logger example

--- lib/logger.py
+++ lib/logger.py
@@ -4,4 +4,5 @@ from datetime import datetime
 logger = getLogger(__name__)
 format = "[%(asctime)s] [%(level)s] [%(file)s] [%(type)s] %(message)s"
 basicConfig(filename=f"logs/{str(datetime.now()).split(" ")[0]}.log", level=NOTSET, encoding="UTF-8", filemode="a", format=format)
-logger.info("test", extra={"file": "logger.py", "type": "logger", "level": "INFO"})
+
+# logger.info("test", extra={"file": "logger.py", "type": "logger", "level": "INFO"})
