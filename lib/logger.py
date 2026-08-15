81c740265b300cf4fac6b183d229396e18bc7f8f
elixirelix <nathanael.martin210918@gmail.com>
2026-08-16 00:00:19 +0200

    fix: fixing all logger

--- lib/logger.py
+++ lib/logger.py
@@ -1,8 +1,28 @@
-from logging import getLogger, basicConfig, NOTSET
+from logging import getLogger, DEBUG, Formatter, FileHandler, StreamHandler, Filter
 from datetime import datetime
+from os import getenv
 
-logger = getLogger(__name__)
-format = "[%(asctime)s] [%(level)s] [%(file)s] [%(type)s] %(message)s"
-basicConfig(filename=f"logs/{str(datetime.now()).split(" ")[0]}.log", level=NOTSET, encoding="UTF-8", filemode="a", format=format)
+class Filtering(Filter):
+    def filter(self, record):
+        return record.name == "root"
 
-# logger.info("test", extra={"file": "logger.py", "type": "logger", "level": "INFO"})
+logger = getLogger(getenv("LOGGER_NAME"))
+logger.setLevel(DEBUG)
+
+formatter = Formatter(
+    "[%(asctime)s] [%(levelname)s] [%(name)s] [%(message)s]"
+)
+
+file_handler = FileHandler(
+    f'logs/{str(datetime.now()).split(" ")[0]}.log'
+)
+file_handler.setLevel(DEBUG)
+file_handler.setFormatter(formatter)
+
+console_handler = StreamHandler()
+console_handler.setLevel(DEBUG)
+console_handler.setFormatter(formatter)
+console_handler.addFilter(Filtering())
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
