439c80401a689bbcb6d35b255bcb314a403433be
elixirelix <nathanael.martin210918@gmail.com>
2026-08-16 00:15:57 +0200

    fix: adding console type for logger

--- lib/logger.py
+++ lib/logger.py
@@ -8,29 +8,39 @@ class Filtering(Filter):
         return record.name == "root"
 
 class CustomFormatter(Formatter):
+    def __init__(self, is_console=False):
+        super().__init__()
+        self.is_console = is_console
+
     def format(self, record):
         if hasattr(record, "file"):
-            self._style._fmt = "[%(asctime)s] [%(levelname)s] [%(name)s] [%(file)s] [%(message)s]"
+            self._style._fmt = (
+                "[%(asctime)s] [%(levelname)s] "
+                "[%(name)s] [%(file)s] [%(message)s]"
+            )
         else:
-            self._style._fmt = "[%(asctime)s] [%(levelname)s] [%(name)s] [%(message)s]"
+            self._style._fmt = (
+                "[%(asctime)s] [%(levelname)s] "
+                "[%(name)s] [%(message)s]"
+            )
 
         return super().format(record)
-
 
 logger = getLogger(getenv("LOGGER_NAME"))
 logger.setLevel(DEBUG)
 
-formatter = CustomFormatter()
+file_formatter = CustomFormatter(is_console=False)
+console_formatter = CustomFormatter(is_console=True)
 
 file_handler = FileHandler(
     f'logs/{str(datetime.now()).split(" ")[0]}.log'
 )
 file_handler.setLevel(DEBUG)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(file_formatter)
 
 console_handler = StreamHandler()
 console_handler.setLevel(DEBUG)
-console_handler.setFormatter(formatter)
+console_handler.setFormatter(console_formatter)
 console_handler.addFilter(Filtering())
 
 logger.addHandler(file_handler)
