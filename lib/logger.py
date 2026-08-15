d0c8664628c6632a61bfa1152551ae874a336e2c
elixirelix <nathanael.martin210918@gmail.com>
2026-08-15 20:35:38 +0200

    fix: logger

--- lib/logger.py
+++ lib/logger.py
@@ -1,1 +1,7 @@
-import logging
+from logging import getLogger, basicConfig, NOTSET
+from datetime import datetime
+
+logger = getLogger(__name__)
+format = "[%(asctime)s] [%(level)s] [%(file)s] [%(type)s] %(message)s"
+basicConfig(filename=f"logs/{str(datetime.now()).split(" ")[0]}.log", level=NOTSET, encoding="UTF-8", filemode="a", format=format)
+logger.info("test", extra={"file": "logger.py", "type": "logger", "level": "INFO"})
