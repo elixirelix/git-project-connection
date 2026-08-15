1c331ff2a4f416a89a1e2f738dc4052bf24f8e56
elixirelix <nathanael.martin210918@gmail.com>
2026-08-15 22:38:28 +0200

    fix: server type for push

--- server.py
+++ server.py
@@ -1,16 +1,51 @@
-from os import getenv
 from uvicorn import Config, Server
 from fastapi import FastAPI
+from discord import Embed, Colour
+from os import getenv
 from bot import bot
 app = FastAPI()
 
 @app.post("/github/push")
 async def push(data: dict):
-    repo_url, repo_id = data["repository"]["html_url"], data["repository"]["id"]
+    repo_url, repo_id, repo_name = data["repository"]["html_url"], data["repository"]["id"], data["repository"]["name"]
+    commit_list = data["commits"]
+
     forced_push = data["forced"]
+    avatar = f"https://avatars.githubusercontent.com/u/{data["sender"]["id"]}?v=4"
+    repo_infos = f"""Repository Informations:
+Repository URl: {repo_url}
+Repository ID: {repo_id}
+Total Commit: {len(commit_list)}
+Pushed by: {data["sender"]["login"]}
+    """
+
+    embed_push = Embed(
+        title=f"[NEW PUSH] - {repo_name}",
+        description=repo_infos,
+        color=Colour.blue() if not forced_push else Colour.dark_red()
+    )
     
-    channel = bot.get_channel(getenv("CHANNEL_MESSAGE_ID"))
-    await channel.send("test")
+    for commits in commit_list:
+        commit_id, commit_message = commits["id"], commits["message"]
+        commit_add, commit_remove, commit_modified = commits["added"], commits["removed"], commits["modified"]
+        commit_infos =  f"""Files edited: {len(commit_add) + len(commit_remove) + len(commit_modified)}
+Add: {"``" + "`` ``".join(commit_add) + "``" if len(commit_add) > 0 else "No files"}
+Modified: {"``" + "`` ``".join(commit_modified) + "``" if len(commit_modified) > 0 else "No files"}
+Remove: {"``" + "`` ``".join(commit_remove) + "``" if len(commit_remove) > 0 else "No files"}
+
+Commit time: {commits["timestamp"]}
+Commited by: {commits["committer"]["name"]}
+"""
+
+        embed_push.add_field(name=f"{commit_message} - {commit_id}", value=commit_infos)
+
+    embed_push.set_author(name=f"Sender Commit - {data["sender"]["login"]}", icon_url=avatar)
+    embed_push.set_thumbnail(url=f"https://opengraph.githubassets.com/1/{data["repository"]["owner"]["login"]}/{repo_name}")
+    embed_push.set_footer(text=f"{data["head_commit"]["timestamp"]}")
+
+    channel = bot.get_channel(int(getenv("CHANNEL_MESSAGE_ID")))
+    await channel.send(embed=embed_push)
+
     return {"status": "ok"}
 
 @app.get("/")
